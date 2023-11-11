272656d53a809591c169b3d2b6a973dbdcfe771d
Dragoș Efrem <101568589+dragosefrem@users.noreply.github.com>
2023-11-12 00:43:57 +0200

    Included an example containing the three type of entries

--- Homework/src/main.cpp
+++ Homework/src/main.cpp
@@ -1,4 +1,6 @@
 #include "LoginEntry.h"
+#include "CreditCardEntry.h"
+#include "SecureNoteEntry.h"
 #include "Database.h"
 #include <iostream>
 #include <cstring>
@@ -14,8 +16,24 @@ int main() {
     // Create a login entry
     LoginEntry entry("Google", "google.com", "user", "pass");
 
+    // create a credit card entry
+
+    CreditCardEntry ccEntry("Visa", "1234 5678 9012 3456", "John Doe", "01/23", "123");
+
+    // create a secure note entry
+
+    SecureNoteEntry snEntry("My Note", "This is a note");
+
     // Add the entry to the database
     db.addEntry(&entry);
+
+    // Add the credit card entry to the database
+
+    db.addEntry(&ccEntry);
+
+    // Add the secure note entry to the database
+
+    db.addEntry(&snEntry);
 
    // iterate through database and print out entries
     
