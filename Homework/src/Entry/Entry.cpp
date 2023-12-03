a81d54af10ca788b5a8a2ae61ee5e673cb59d714
Dragoș Efrem <101568589+dragosefrem@users.noreply.github.com>
2023-12-03 22:49:02 +0200

    Modified base class Entry implementation to add mutex and smart pointers support

--- Homework/src/Entry/Entry.cpp
+++ Homework/src/Entry/Entry.cpp
@@ -1,44 +1,40 @@
+#include "Database.h"
 #include "Entry.h"
+#include <iostream>
+#include <cstring>
+#include <memory>
 
 using namespace Vaultify::Entries;
+using namespace Vaultify;
 
 uint32_t Entry::idCounter = 1; 
 
-Entry::Entry() 
-    : id(idCounter++)
-{
-    
+std::mutex Entry::mtx;
+
+Entry::Entry() {    
+    mtx.lock();
+    id = idCounter++;
+    mtx.unlock();
     std::cout << "(constructor): Constructor for entry " << id << " called\n";
 }
 
-Entry::Entry(const char* title) 
-    : Entry()
-{
-    this->title = new char[strlen(title) + 1];
-    strcpy(this->title, title);
+Entry::Entry(const char* title) : Entry() {
+    this->title = std::make_unique<char[]>(strlen(title) + 1);
+    strcpy(this->title.get(), title);
 }
 
-Entry::Entry(const Entry& other) 
-    : id(idCounter++), title(new char[strlen(other.title) + 1])
-{
+Entry::Entry(const Entry& other) : id(idCounter++), title(std::make_unique<char[]>(strlen(other.title.get()) + 1)) {
     std::cout << "(copy_constructor): Copied contents of entry " << other.id << " to entry " << id << "\n";
-    strcpy(this->title, other.title);
+    strcpy(this->title.get(), other.title.get());
 }
 
-Entry::Entry(Entry&& other) noexcept 
-    : id(other.id), title(other.title)
-{
+Entry::Entry(Entry&& other) noexcept : id(other.id), title(std::move(other.title)) {
     std::cout << "(move_constructor): Moved contents of entry " << id << "\n";
-    other.title = nullptr;
     other.id = MOVED_ID_VALUE;
 }
 
 Entry::~Entry() {
-    if (id == MOVED_ID_VALUE)
-        std::cout << "(destructor): Destructor for already moved entry " << id << " called\n";
-    else {
-        std::cout << "(destructor): Destructor for entry " << id << " called\n";
-    }
+    std::cout << "(destructor): Destructor for entry " << id << " called\n";
 }
 
 int Entry::getId() const {
@@ -46,11 +42,33 @@ int Entry::getId() const {
 }
 
 const char* Entry::getTitle() const {
-    return this->title;
+    return this->title.get();
 }
 
 void Entry::setTitle(const char* new_title) {
-    delete[] this->title;
-    this->title = new char[strlen(new_title) + 1];
-    strcpy(this->title, new_title);
+    this->title = std::make_unique<char[]>(strlen(new_title) + 1);
+    strcpy(this->title.get(), new_title);
+}
+
+void Entry::setParentDatabase(std::weak_ptr<Database> parentDatabase) {
+    this->parentDatabase = parentDatabase;
+}
+
+std::shared_ptr<Database> Entry::getParentDatabase() const {
+    return this->parentDatabase.lock();
+}
+
+Entry& Entry::operator=(const Entry& other) {
+            if (this != &other) {
+              title = std::make_unique<char[]>(strlen(other.title.get()) + 1);
+              strcpy(title.get(), other.title.get());
+        }
+        return *this;
+    }
+
+Entry& Entry::operator=(Entry&& other) noexcept {
+    if (this != &other) {
+        title = std::move(other.title);
+    }
+    return *this;
 }
